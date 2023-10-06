921529ef4678794c12762a8dd9b540dd20106302
HidemaruOwO <hidemal@outlook.jp>
2023-10-06 17:21:15 +0900

    :snowman: Change log utils (log/util.go)

--- log/util.go
+++ log/util.go
@@ -6,20 +6,22 @@ import (
 	"os"
 )
 
-func Infof(format string, a ...any) {
-	logType := color.HiCyanString("INFO")
-	fmt.Printf("[%s] %s", logType, fmt.Sprintf(format, a...))
-}
-
 func Error(err error) {
 	logType := color.RedString("ERROR")
 	errorMessage := color.RedString(err.Error())
-	fmt.Fprintf(os.Stderr, "[%s] %s", logType, errorMessage)
+	fmt.Fprintf(os.Stderr, "[%s] %s\n", logType, errorMessage)
 }
 
-func ErrorExit(err error) {
-	Error(err)
+func Critical(err error) {
+	logType := color.HiMagentaString("CRITICAL")
+	errorMessage := color.RedString(err.Error())
+	fmt.Fprintf(os.Stderr, "[%s] %s\n", logType, errorMessage)
 	panic(err)
+}
+
+func Infof(format string, a ...any) {
+	logType := color.HiCyanString("INFO")
+	fmt.Printf("[%s] %s", logType, fmt.Sprintf(format, a...))
 }
 
 func Warnf(format string, a ...any) {
@@ -27,14 +29,29 @@ func Warnf(format string, a ...any) {
 	fmt.Printf("[%s] %s", logType, fmt.Sprintf(format, a...))
 }
 
-func Criticalf(format string, a ...any) {
-	logType := color.HiMagentaString("CRITICAL")
-	fmt.Printf("[%s] %s", logType, fmt.Sprintf(format, a...))
-}
-
 func Debugf(isDebug bool, format string, a ...any) {
 	if isDebug {
 		logType := color.HiGreenString("DEBUG")
 		fmt.Printf("[%s] %s", logType, fmt.Sprintf(format, a...))
+	}
+}
+
+func Info(format string, a ...interface{}) {
+	logType := color.HiCyanString("INFO")
+	message := fmt.Sprintf(format, a...)
+	fmt.Printf("[%s] %s\n", logType, message)
+}
+
+func Warn(format string, a ...interface{}) {
+	logType := color.HiYellowString("WARN")
+	message := fmt.Sprintf(format, a...)
+	fmt.Printf("[%s] %s\n", logType, message)
+}
+
+func Debug(isDebug bool, format string, a ...interface{}) {
+	if isDebug {
+		logType := color.HiGreenString("DEBUG")
+		message := fmt.Sprintf(format, a...)
+		fmt.Printf("[%s] %s\n", logType, message)
 	}
 }
